7d0703509dc60788824f33e8a97a900e1830b0aa
priyags619 <118427427+priyags619@users.noreply.github.com>
2023-01-11 21:48:24 +0530

    array

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import '../styles/App.css';
-const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"]`) // do not change
-const Join = (props) =>{
-  return(
-    <div id ="join">
-      {/* Access prop 'words' and print it using .join like words.join(',')*/}
-    </div>
-  )
-}
-const App = () => {
+import React from "react";
+import "../styles/App.css";
+const arr = JSON.parse(
+  window.localStorage.getItem("props") || `["hello","world"]`
+); // do not change
+
+const Join = (props) => {
+  const { words } = props;
 
   return (
-    <div id="main">
-      <Join  />
+    <div id="join">
+      {/* Access prop 'words' and print it using .join like words.join(',')*/}
+      {words.join(",")}
     </div>
-  )
-}
-
+  );
+};
+const App = () => {
+  return (
+    <div id="main">
+      <Join words={arr} />
+    </div>
+  );
+};
 
 export default App;
